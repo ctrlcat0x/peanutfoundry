2f18081fa12dcf7e96ba1f4f25b332e72a97ab67
ctrlcat0x <developer.sahilrana@gmail.com>
2025-07-07 10:42:13 +0530

    Remove unused CommandIcon import from search component to streamline code.

--- components/search.tsx
+++ components/search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CommandIcon, FileIcon, SearchIcon } from "lucide-react";
+import { FileIcon, SearchIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import {
   Dialog,
